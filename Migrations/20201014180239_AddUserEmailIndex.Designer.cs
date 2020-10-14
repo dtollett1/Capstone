fcb9ee463502084bde879ecfe1de4be12d73f92d
Dustin Tollett <dustintollett@gmail.com>
2020-10-14 14:25:34 -0400

    adds uniqe index to prevent duplicate user email

--- Migrations/20201014180239_AddUserEmailIndex.Designer.cs
+++ Migrations/20201014180239_AddUserEmailIndex.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using CapstoneProject.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace CapstoneProject.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20201014180239_AddUserEmailIndex")]
+    partial class AddUserEmailIndex
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
