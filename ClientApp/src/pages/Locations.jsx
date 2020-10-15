b4478aa6b7c36bd148e61152ed946404aab45811
Dustin Tollett <dustintollett@gmail.com>
2020-10-15 18:47:12 -0400

    integrated react mapgl into application

--- ClientApp/src/pages/Locations.jsx
+++ ClientApp/src/pages/Locations.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import ReactMapGL, { NavigationControl } from 'react-map-gl'
+
 import logo from '../images/onlocation.png'
 import map from '../images/map.png'
 import { Link } from 'react-router-dom'
@@ -7,6 +9,14 @@ import { Link } from 'react-router-dom'
 export function Locations() {
   const [locations, setLocations] = useState([])
   const [filterText, setFilterText] = useState('')
+
+  const [viewport, setViewport] = useState({
+    width: 327,
+    height: 264,
+    latitude: 27.77101804911986,
+    longitude: -82.66090611749074,
+    zoom: 9.8,
+  })
 
   useEffect(
     function () {
@@ -38,6 +48,18 @@ export function Locations() {
             setFilterText(event.target.value)
           }}
         />
+        <section className="map">
+          <ReactMapGL
+            style={{ position: 'absolute' }}
+            {...viewport}
+            onViewportChange={setViewport}
+            mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+          >
+            <div style={{ position: 'absolute', right: 0 }}>
+              <NavigationControl />
+            </div>
+          </ReactMapGL>
+        </section>
         <ul className="results">
           {locations.map((location) => (
             <li key={location.id}>
